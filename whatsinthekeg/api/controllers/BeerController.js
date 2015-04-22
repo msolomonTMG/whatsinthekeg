b5f3de04d1a8614fed3144462d65e56ecdfe08e2
Mike Solomon <msolomon@thrillist.com>
2015-04-22 17:45:27 -0400

    First pages commit

---
+++ whatsinthekeg/api/controllers/BeerController.js
@@ -0,0 +1,11 @@
+/**
+ * BeerController
+ *
+ * @description :: Server-side logic for managing beers
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
